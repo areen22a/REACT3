ecc80e8edbfccdd797083f688772a18b1737d1db
areen22a <153274443+areen22a@users.noreply.github.com>
2024-02-24 22:59:23 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import About from './components/About/About'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <About />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
